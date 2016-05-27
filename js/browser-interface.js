a27ea7fb2c72b72cb8cea4a8ecfa83d101d31a2e
Alex H <alexhagge128@gmail.com>
2016-05-27 10:40:27 -0700

    installed bower/refactored js

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,11 +1,12 @@
-//API Key
-var apiKey = "3ef915debb8c42586ba3613b4029db48f18b57e9"
+var Repos = require('./../js/github.js').Repos;
 
-//User Logic
 $(document).ready(function() {
-  $('#userName').click(function() {
+  var newUserRepo = new Repos();
+  $('#findUser').click(function() {
     var user = $('#userName').val();
     $('#userName').val("");
-    $('.showUser').text("This is " + user + ".")
-  })
-})
+    newUserRepo.getRepos(user);
+
+    $("#repoHere").text(response);
+  });
+});
--- js/github.js
+++ js/github.js
@@ -1,13 +1,13 @@
-// var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=text&paragraphs=1&words=' + Math.floor((Math.random() * 100) + 1)).then(function(response) {
-//   return dinoAnswer = response;
-// });
+var apiKey = require('./../.env').apiKey;
 
-var apiKey = "3ef915debb8c42586ba3613b4029db48f18b57e9"
+exports.Repos = function() {
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/VARIABLEOFUSERINPUT?access_token=' + apiKey).then(function(response){
+}
+
+exports.Repos.prototype.getRepos = function(user) {
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
-};
+}
