8f79cc15f7698be2fb91572a020651a218efd6ff
Alex H <alexhagge128@gmail.com>
2016-05-27 12:12:12 -0700

    added api repos

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -2,11 +2,12 @@ var Repos = require('./../js/github.js').Repos;
 
 $(document).ready(function() {
   var newUserRepo = new Repos();
+
   $('#findUser').click(function() {
     var user = $('#userName').val();
     $('#userName').val("");
     newUserRepo.getRepos(user);
+    //$('#repoHere').text();
 
-    $("#repoHere").text(response);
   });
 });
--- js/github.js
+++ js/github.js
@@ -5,8 +5,9 @@ exports.Repos = function() {
 }
 
 exports.Repos.prototype.getRepos = function(user) {
-  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
