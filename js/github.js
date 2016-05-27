3253f7a74879c93a8714a60b562730d11ec86e71
Alex H <alexhagge128@gmail.com>
2016-05-27 14:10:54 -0700

    applied html tables

--- js/github.js
+++ js/github.js
@@ -10,20 +10,26 @@ exports.Repos.prototype.getUser = function(user) {
   $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
 
     //console.log(response);
-    $("#userHere").append("<h2>" + user + "</h2>");
+    reponse.forEach(function(account) {
+      $("#userHere").append("<h2>" + user + "</h2>");
+    });
 
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 }
 
-//Call for Repo Name/Description
+//Call for Repo Name/Descriptions
 exports.Repos.prototype.getRepos = function(user) {
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
 
     response.forEach(function(repo) {
-      $("#repoHere").append("<h4>" + repo.name + "</h4>");
-      $("#repoHere").append("<h4>" + repo.description + "</h4>");
+
+      if(repo.description.length>1){
+        $("#repoHere").append("<tr><td>" + repo.name + "</td><td>" + repo.description + "</td></tr>");
+      } else {
+        $("#repoHere").append("<tr><td>" + repo.name + "</td><td></td></tr>");
+      }
     });
 
   }).fail(function(error){
